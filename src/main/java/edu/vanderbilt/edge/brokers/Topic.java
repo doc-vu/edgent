7512a65ead1ba6edcff361e1aef69c3b63a09521
kharesp <shweta.p.khare@gmail.com>
2017-07-05 10:09:26 -0500

    initial commit

---
+++ src/main/java/edu/vanderbilt/edge/brokers/Topic.java
@@ -0,0 +1,51 @@
+package edu.vanderbilt.edge.brokers;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
+
+public class Topic implements Runnable {
+	private String topic;
+
+	private ZMQ.Context context;
+	private ZMQ.Socket receiveSocket;
+	private ZMQ.Socket sendSocket;
+
+	private int receivePort;
+	private int sendPort;
+	
+	private volatile boolean stopped= false;
+	
+	public Topic(String topic, ZMQ.Context context,int receivePort,int sendPort){
+		this.topic= topic;
+
+		this.context=context;
+		receiveSocket= context.socket(ZMQ.SUB);
+		sendSocket= context.socket(ZMQ.PUB);
+
+		this.receivePort=receivePort;
+		this.sendPort=sendPort;
+	}
+
+	@Override
+	public void run() {
+		receiveSocket.bind(String.format("tcp://*:%d",receivePort));
+		receiveSocket.subscribe(topic.getBytes());
+
+		sendSocket.bind(String.format("tcp://*:%d",sendPort));
+		while(!stopped){
+			ZMsg receivedMsg = ZMsg.recvMsg(receiveSocket);
+			String msgTopic = new String(receivedMsg.getFirst().getData());
+		    byte[] msgContent = receivedMsg.getLast().getData();
+		    sendSocket.sendMore(msgTopic);
+	        sendSocket.send(msgContent);
+		}
+		receiveSocket.close();
+		sendSocket.close();
+	}
+
+	
+	public void stop(){
+		stopped=true;
+	}
+	
+}
---
+++ src/main/java/edu/vanderbilt/edge/util/PortList.java
@@ -0,0 +1,51 @@
+package edu.vanderbilt.edge.util;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PortList {
+	private static Queue<Integer> pool; 
+	private static PortList instance=null;
+	/*
+	 * User port range: 1024-49151 
+	 */
+	private static int START_RANGE= 5000;
+	private static int END_RANGE= 25000; 
+
+	/**
+	 * Singleton instance 
+	 */
+	private PortList(){}
+
+
+	/**
+	 * Method to access the singleton instance of PortList 
+	 * @return singleton instance of PortList
+	 */
+	public static PortList getInstance(){
+		if(instance==null){
+			instance= new PortList();
+			pool= new LinkedList<Integer>();
+			for(int i=START_RANGE;i<END_RANGE;i++){
+				pool.offer(i);
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * Acquires an unused port number 
+	 * @return unused port number
+	 */
+	public Integer acquire(){
+		return pool.poll();
+	}
+	
+	/**
+	 * Returns a free port number to be reused. 
+	 */
+	public void release(Integer port){
+		pool.offer(port);
+	}
+
+}
